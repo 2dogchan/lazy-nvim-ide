fb88955b3137d96cf3584d10e511f89e6601c79a
2dogchan <2dogchan@gmail.com>
2023-08-03 18:47:45 +0800

    chore: add fold preview keybinding

--- lua/plugins/folding.lua
+++ lua/plugins/folding.lua
@@ -25,6 +25,7 @@ return {
         return { "treesitter", "indent" }
       end,
     },
+    --stylua: ignore
     init = function()
       vim.o.foldcolumn = "1" -- '0' is not bad
       vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
@@ -32,12 +33,9 @@ return {
       vim.o.foldenable = true
       vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 
-      vim.keymap.set("n", "zR", function()
-        require("ufo").openAllFolds()
-      end)
-      vim.keymap.set("n", "zM", function()
-        require("ufo").closeAllFolds()
-      end)
+      vim.keymap.set("n", "zR", function() require("ufo").openAllFolds() end)
+      vim.keymap.set("n", "zM", function() require("ufo").closeAllFolds() end)
+      vim.keymap.set('n', 'zK', function() require('ufo').peekFoldedLinesUnderCursor() end)
     end,
   },
   -- Folding preview, by default h and l keys are used.
