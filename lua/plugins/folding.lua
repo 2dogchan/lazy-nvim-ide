573f5343338ee244f6037da7e69f9c4d88c340cd
2dogchan <2dogchan@gmail.com>
2023-08-14 09:55:03 +0800

    chore: update fold-preview keybindings

--- lua/plugins/folding.lua
+++ lua/plugins/folding.lua
@@ -35,17 +35,7 @@ return {
 
       vim.keymap.set("n", "zR", function() require("ufo").openAllFolds() end)
       vim.keymap.set("n", "zM", function() require("ufo").closeAllFolds() end)
-      vim.keymap.set('n', 'zK', function() require('ufo').peekFoldedLinesUnderCursor() end)
+      vim.keymap.set('n', 'zk', function() require('ufo').peekFoldedLinesUnderCursor() end)
     end,
-  },
-  -- Folding preview, by default h and l keys are used.
-  -- On first press of h key, when cursor is on a closed fold, the preview will be shown.
-  -- On second press the preview will be closed and fold will be opened.
-  -- When preview is opened, the l key will close it and open fold. In all other cases these keys will work as usual.
-  {
-    "anuvyklack/fold-preview.nvim",
-    event = "BufReadPost",
-    dependencies = "anuvyklack/keymap-amend.nvim",
-    config = true,
   },
 }
