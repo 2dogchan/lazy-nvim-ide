12eef3b24c58facd75a15d7922295602750917f1
2dogchan <2dogchan@gmail.com>
2023-08-16 16:03:41 +0800

    chore: add bufferline prompt prefix

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -3,6 +3,7 @@ return {
   keys = {
     { "<leader>b0", "<Cmd>BufferLineGoToBuffer 1<CR>", desc = "First Buffer" },
     { "<leader>b$", "<Cmd>BufferLineGoToBuffer $<CR>", desc = "Last Buffer" },
+    { "<leader>bc", desc = "+close" },
     { "<leader>bc0", "<Cmd>BufferLineCloseLeft<CR>", desc = "Close Left Buffer" },
     { "<leader>bc$", "<Cmd>BufferLineCloseRight<CR>", desc = "Close Right Buffer" },
   },
